7ad5189c8c5cd528d7a22c414062f033688c7135
Logan Delaney <logan_delaney@yahoo.com>
2024-09-19 22:57:51 -0400

    Exercises 1-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  let increment = num1;
+  for (i = num1; i < num2; i++) {
+    increment = increment + i + 1;
+  }
+  return increment;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  toCel = (fTemp - 32) * (5 / 9);
+  roundC = Math.round(toCel * 10) / 10;
+  return roundC
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  toFar = cTemp * (9 / 5) + 32;
+  roundF = Math.round(toFar * 10) / 10;
+  return roundF
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
